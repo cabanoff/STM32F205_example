4831fadd369d016edac2c4ff7ef24ebf64ee8462
Aleksei Kabanov <al.al.kabanov@gmail.com>
2018-02-16 17:17:28 +0300

    frequency checked in Simbia

---
+++ Stage1/Ver1_2/Src/main.c
@@ -0,0 +1,497 @@
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f2xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+/*
+RNG_HandleTypeDef hrng;
+
+TIM_HandleTypeDef htim1;
+
+UART_HandleTypeDef huart1;
+
+WWDG_HandleTypeDef hwwdg;
+*/
+/* Timer handler declaration */
+TIM_HandleTypeDef               TimHandle;
+
+/* Timer Output Compare Configuration Structure declaration */
+TIM_OC_InitTypeDef              sPWMConfig;
+/* Timer Break Configuration Structure declaration */
+TIM_BreakDeadTimeConfigTypeDef sBreakConfig;
+
+/* Counter Prescaler value */
+uint32_t uwPrescalerValue = 0;
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+#define  PERIOD_VALUE       2790  /* Period Value (2791 - 1) */
+#define  PULSE1_VALUE       1395  /* Capture Compare 1 Value  */ //
+#define  DEAD_TIME          0xc5
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+//static void MX_GPIO_Init(void);
+//static void MX_USART1_UART_Init(void);
+static void MX_TIM1_Init(void);
+//static void MX_RNG_Init(void);
+//static void MX_WWDG_Init(void);
+//static void MX_NVIC_Init(void);
+                                    
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+                                
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  *
+  * @retval None
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  //MX_GPIO_Init();
+ // MX_USART1_UART_Init();
+  MX_TIM1_Init();
+ // MX_RNG_Init();
+ // MX_WWDG_Init();
+
+  /* Initialize interrupts */
+ // MX_NVIC_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 240;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 5;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  /*
+  //Configure the Systick interrupt time 
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+  //Configure the Systick    
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  //SysTick_IRQn interrupt configuration
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+  */
+}
+
+/**
+  * @brief NVIC Configuration.
+  * @retval None
+  */
+/*
+static void MX_NVIC_Init(void)
+{
+  //USART1_IRQn interrupt configuration
+  HAL_NVIC_SetPriority(USART1_IRQn, 2, 0);
+  HAL_NVIC_EnableIRQ(USART1_IRQn);
+  //TIM1_CC_IRQn interrupt configuration
+  HAL_NVIC_SetPriority(TIM1_CC_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(TIM1_CC_IRQn);
+  //TIM1_UP_TIM10_IRQn interrupt configuration
+  HAL_NVIC_SetPriority(TIM1_UP_TIM10_IRQn, 3, 0);
+  HAL_NVIC_EnableIRQ(TIM1_UP_TIM10_IRQn);
+  //RCC_IRQn interrupt configuration
+  HAL_NVIC_SetPriority(RCC_IRQn, 4, 0);
+  HAL_NVIC_EnableIRQ(RCC_IRQn);
+}
+*/
+/* RNG init function */
+/*
+static void MX_RNG_Init(void)
+{
+
+  hrng.Instance = RNG;
+  if (HAL_RNG_Init(&hrng) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+*/
+/* TIM1 init function */
+static void MX_TIM1_Init(void)
+{
+   /*##-1- Configure the TIM peripheral #######################################*/
+  /* ---------------------------------------------------------------------------
+  1/ Generate 3 complementary PWM signals with 3 different duty cycles:
+  
+    TIM1 input clock (TIM1CLK) is set to 2 * APB2 clock (PCLK2), since APB2 
+    prescaler is different from 1.   
+    TIM1CLK = 2 * PCLK2  
+    PCLK2 = HCLK / 2 
+    => TIM1CLK = 2 * (HCLK / 2) = HCLK = SystemCoreClock
+  
+    TIM1CLK is fixed to SystemCoreClock, the TIM1 Prescaler is set to have
+    TIM1 counter clock = 18MHz..
+
+    The objective is to generate PWM signal at 10 KHz:
+    - TIM1_Period = (SystemCoreClock / 10000) - 1
+
+    The Three Duty cycles are computed as the following description: 
+
+    The channel 1 duty cycle is set to 50% so channel 1N is set to 50%.
+    The channel 2 duty cycle is set to 25% so channel 2N is set to 75%.
+    The channel 3 duty cycle is set to 12.5% so channel 3N is set to 87.5%.
+    
+    The Timer pulse is calculated as follows:
+      - ChannelxPulse = DutyCycle * (TIM1_Period - 1) / 100
+
+  2/ Insert a dead time equal to (11/SystemCoreClock) ns
+
+  3/ Configure the break feature, active at High level, and using the automatic 
+     output enable feature
+
+  4/ Use the Locking parameters level1. 
+  
+  Note: 
+     SystemCoreClock variable holds HCLK frequency and is defined in system_stm32f2xx.c file.
+     Each time the core clock (HCLK) changes, user had to update SystemCoreClock 
+     variable value. Otherwise, any configuration based on this variable will be incorrect.
+     This variable is updated in three ways:
+      1) by calling CMSIS function SystemCoreClockUpdate()
+      2) by calling HAL API function HAL_RCC_GetSysClockFreq()
+      3) each time HAL_RCC_ClockConfig() is called to configure the system clock frequency   
+  --------------------------------------------------------------------------- */
+  
+  
+  /* Select the Timer instance */
+  TimHandle.Instance = TIM1;
+  
+  TimHandle.Init.Prescaler         = 0;
+  TimHandle.Init.Period            = PERIOD_VALUE;
+  TimHandle.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;
+  TimHandle.Init.CounterMode       = TIM_COUNTERMODE_UP;
+  TimHandle.Init.RepetitionCounter = 0;
+  TimHandle.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;//TIM_AUTORELOAD_PRELOAD_ENABLE;
+    
+  if(HAL_TIM_PWM_Init(&TimHandle) != HAL_OK)
+  {
+    /* Initialization Error */
+    _Error_Handler(__FILE__, __LINE__);
+  }
+  
+  /*##-2- Configure the PWM channels #########################################*/ 
+  /* Common configuration for all channels */
+  sPWMConfig.OCMode       = TIM_OCMODE_PWM1;
+  sPWMConfig.OCPolarity   = TIM_OCPOLARITY_HIGH;
+  sPWMConfig.OCNPolarity  = TIM_OCNPOLARITY_HIGH;
+  sPWMConfig.OCIdleState  = TIM_OCIDLESTATE_SET;
+  sPWMConfig.OCNIdleState = TIM_OCNIDLESTATE_RESET;  
+
+  /* Set the pulse value for channel 1 */
+  sPWMConfig.Pulse = PULSE1_VALUE;  
+  if(HAL_TIM_PWM_ConfigChannel(&TimHandle, &sPWMConfig, TIM_CHANNEL_1) != HAL_OK)
+  {
+    /* Configuration Error */
+    _Error_Handler(__FILE__, __LINE__);
+  }
+  
+ 
+  
+  /* Set the Break feature & Dead time */
+  sBreakConfig.BreakState       = TIM_BREAK_DISABLE;//TIM_BREAK_ENABLE;//
+  sBreakConfig.DeadTime         = DEAD_TIME;
+  sBreakConfig.OffStateRunMode  = TIM_OSSR_ENABLE;
+  sBreakConfig.OffStateIDLEMode = TIM_OSSI_ENABLE;
+  sBreakConfig.LockLevel        = TIM_LOCKLEVEL_OFF;//TIM_LOCKLEVEL_1;//
+  sBreakConfig.BreakPolarity    = TIM_BREAKPOLARITY_HIGH;
+  sBreakConfig.AutomaticOutput  = TIM_AUTOMATICOUTPUT_ENABLE;//TIM_AUTOMATICOUTPUT_DISABLE;
+  
+  if(HAL_TIMEx_ConfigBreakDeadTime(&TimHandle, &sBreakConfig) != HAL_OK)
+  {
+    /* Configuration Error */
+    _Error_Handler(__FILE__, __LINE__);
+  }
+  /*##-3- Start PWM signals generation #######################################*/ 
+  /* Start channel 1 */
+  if(HAL_TIM_PWM_Start(&TimHandle, TIM_CHANNEL_1) != HAL_OK)
+  {
+    /* Starting Error */
+    _Error_Handler(__FILE__, __LINE__);
+  }
+  /* Start channel 1N */
+  if(HAL_TIMEx_PWMN_Start(&TimHandle, TIM_CHANNEL_1) != HAL_OK)
+  {
+    /* Starting Error */
+    _Error_Handler(__FILE__, __LINE__);
+  }    
+  
+/*
+  TIM_MasterConfigTypeDef sMasterConfig;
+  TIM_OC_InitTypeDef sConfigOC;
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
+
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 2791;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 100;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 1083;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_LOW;
+  sConfigOC.OCFastMode = TIM_OCFAST_ENABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_SET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0xc5;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  HAL_TIM_MspPostInit(&htim1);
+*/
+}
+
+/* USART1 init function */
+/*
+static void MX_USART1_UART_Init(void)
+{
+
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+*/
+/* WWDG init function */
+/*
+static void MX_WWDG_Init(void)
+{
+
+  hwwdg.Instance = WWDG;
+  hwwdg.Init.Prescaler = WWDG_PRESCALER_1;
+  hwwdg.Init.Window = 64;
+  hwwdg.Init.Counter = 64;
+  hwwdg.Init.EWIMode = WWDG_EWI_DISABLE;
+  if (HAL_WWDG_Init(&hwwdg) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+*/
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+//static void MX_GPIO_Init(void)
+//{
+//
+//  /* GPIO Ports Clock Enable */
+//  __HAL_RCC_GPIOH_CLK_ENABLE();
+//  __HAL_RCC_GPIOA_CLK_ENABLE();
+//  __HAL_RCC_GPIOB_CLK_ENABLE();
+//
+//}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  file: The file name as string.
+  * @param  line: The line in file as a number.
+  * @retval None
+  */
+void _Error_Handler(char *file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+
+
+
